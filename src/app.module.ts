be96b86189c1af01f86646d2df071008d55e29a2
Vlad Popa <vlad20popa@gmail.com>
2024-01-31 10:23:58 +0200

    add race controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RaceController } from './race/race.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, RaceController],
   providers: [AppService],
 })
 export class AppModule {}
